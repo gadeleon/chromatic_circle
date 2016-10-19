3b0936269ceac4eceee37f2ec6c82c891aef8853
Gabriel Deleon <gadeleon@Gabriels-MacBook-Pro-2.local>
2016-10-19 11:43:01 -0400

    added test first attempt at test case

--- test/test_quiz.py
+++ test/test_quiz.py
@@ -3,3 +3,26 @@ Unit tests for quiz.py
 '''
 
 import unittest
+
+import quiz
+
+class RunKeyTestCase(unittest.TestCase):
+
+    '''Tests for run_report'''
+
+    def setUp(self):
+        '''
+        Called before testing
+        '''
+        pass
+
+    def test_incorrect_third(self):
+        '''
+        Assert the Third of C is F (it's actually E)
+        '''
+        self.assertEqual('F', quiz.gen_key('C')[2])
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
