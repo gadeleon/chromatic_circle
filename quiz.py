6afb7d3c9881414c9e54d7c1e28d456fa733ade1
Gabriel Deleon <gad265@Gabriels-MacBook-Pro.local>
2016-10-19 02:22:37 -0400

    gen_key makes a major key now

--- quiz.py
+++ quiz.py
@@ -28,26 +28,21 @@ CHROMA_SCALE = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 def gen_key(note):
     '''
     Using the CHROMA_SCALE for reference, create the key.
+    NOTE: This only works with major. May refactor.
     '''
     note = CHROMA_SCALE.index(note)
-    start = [CHROMA_SCALE[note]]
-    note += 2
-    start.append(CHROMA_SCALE[note])
-    note += 2
-    start.append(CHROMA_SCALE[note])
-    note += 1
-    start.append(CHROMA_SCALE[note])
-    note += 2
-    start.append(CHROMA_SCALE[note])
-    note += 2
-    start.append(CHROMA_SCALE[note])
-    note += 2
-    start.append(CHROMA_SCALE[note])
-    note += 1
-    start.append(CHROMA_SCALE[note % len(CHROMA_SCALE)])
-    return start
+    key = []
+    interval = 1
+    while len(key) < 8:
+        key.append(CHROMA_SCALE[note % len(CHROMA_SCALE)])
+        if interval == 3 or interval == 7:
+            note += 1
+        else:
+            note += 2
+        interval += 1
+    return key
 
 
 if __name__ == '__main__':
-    g = gen_key('C')
+    g = gen_key('A')
     print g
