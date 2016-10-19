8c01d24d29a054df7ecaa373cedb0e7348a4a887
Gabriel Deleon <gadeleon@Gabriels-MacBook-Pro-2.local>
2016-10-19 01:14:55 -0400

    additions
    quiz.py (currently a brain dump)
    test folder added

---
+++ quiz.py
@@ -0,0 +1,23 @@
+'''
+Simple script to correctly calculate the 'correct' notes and chords in a key
+'''
+
+
+'''
+Example:
+
+What is the [3rd] of [C] [Maj]?
+
+Expects the Answer: E (or E Maj)
+
+'''
+
+'''
+
+Brainstorm
+
+* Design function to calculate intervals
+* Build relations such that intervals are calculated correctly
+* Remember Key scale is 2.5 3.5 (C, D, E, F, G, A, B)
+
+'''
