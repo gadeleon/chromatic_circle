0b698310d2dd3cab516fc260c129011f9bda6f96
Gabriel Deleon <gad265@Gabriels-MacBook-Pro.local>
2016-10-19 21:31:54 -0400

    scales incorporated to gen_key()

--- quiz.py
+++ quiz.py
@@ -22,6 +22,7 @@ Brainstorm
 * For lists, major scale intervals are +2, +2, +1, +2, +2, +2, +1
 * Natural Minor Key scale is 1.5, 2.5, 2 (A, B, C, D, E, F, G, A)
 * For lists, natural minor scale intervals are +2, +1, +2, +2, +1, +2, +2
+* The third, sixth, and seventh are flats so 
 * Harmonic Minor Key scale is 1.5, 2.5, 1.5*, .5 *this is 1 stop (A, B, C, D, E, F, G#, A)
 * For lists, harmonic minor scole intervals are +2, +1, +2, +2, +1, +3, +1
 * All the notes C, C#, D, D#, E, F, F#, G, G#, A, A#, B
@@ -32,24 +33,31 @@ MAJOR_HALF = [3, 7]
 MINOR_HALF = [2, 5]
 INTVAL = {'major': [3, 7], 'minor': [2, 5]}
 
-def gen_key(note):
+def gen_key(note, scale):
     '''
     Using the CHROMA_SCALE for reference, create the key.
     NOTE: This only works with major. May refactor.
     '''
     note = CHROMA_SCALE.index(note)
+    scale = scale.lower()
     key = []
     interval = 1
     while len(key) < 8:
         key.append(CHROMA_SCALE[note % len(CHROMA_SCALE)])
-        if interval in INTVAL['major']:
-            note += 1
-        else:
-            note += 2
-        interval += 1
+        try:
+            if interval in INTVAL[scale]:
+                note += 1
+            else:
+                note += 2
+            interval += 1
+        except KeyError:
+            print 'Scale must be "major" or "minor"; {} entered'.format(scale)
+            raise SystemExit
     return key
 
 
 if __name__ == '__main__':
-    g = gen_key('C')
+    g = gen_key('C', 'major')
     print g
+    a = gen_key('A', 'minor')
+    print a
